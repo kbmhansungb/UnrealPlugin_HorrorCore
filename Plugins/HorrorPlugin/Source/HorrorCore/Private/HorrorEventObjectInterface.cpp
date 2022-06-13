1934a7c2f588a936fec78702522149eac19bf94a
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-06-13 23:12:12 +0900

    HorrorEventObjecctInterface추가.

---
+++ Plugins/HorrorPlugin/Source/HorrorCore/Private/HorrorEventObjectInterface.cpp
@@ -0,0 +1,20 @@
+// 2022 06    Bum moo, Kim    Free copyright
+
+
+#include "HorrorEventObjectInterface.h"
+
+// Add default functionality here for any IHorrorEventObjectInterface functions that are not pure virtual.
+
+bool IHorrorEventObjectInterface::isLocalEvent_Implementation(const FHorrorEventStruct& Required)
+{
+	return false;
+}
+
+bool IHorrorEventObjectInterface::CheckCondition_Implementation(const FHorrorEventStruct& Required)
+{
+	return true; 
+}
+
+void IHorrorEventObjectInterface::CallHorrorEvent_Implementation(const FHorrorEventStruct& Required) 
+{ 
+}
---
+++ Plugins/HorrorPlugin/Source/HorrorCore/Public/HorrorEventObjectInterface.h
@@ -0,0 +1,36 @@
+// 2022 06    Bum moo, Kim    Free copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HorrorEventInstance.h"
+#include "HorrorEventObjectInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UHorrorEventObjectInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class HORRORCORE_API IHorrorEventObjectInterface
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent)
+	bool IsLocalEvent(const FHorrorEventStruct& Required);
+	virtual bool isLocalEvent_Implementation(const FHorrorEventStruct& Required);
+
+	UFUNCTION(BlueprintNativeEvent)
+	bool CheckCondition(const FHorrorEventStruct& Required);
+	virtual bool CheckCondition_Implementation(const FHorrorEventStruct& Required);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void CallHorrorEvent(const FHorrorEventStruct& Required);
+	virtual void CallHorrorEvent_Implementation(const FHorrorEventStruct& Required);
+};
