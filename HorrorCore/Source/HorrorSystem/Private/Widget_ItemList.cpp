a0046aacb0e43bd16373834277081ae875a7038c
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-07-24 22:41:32 +0900

    Widget_ItemList의 메서드를 리플리케이션함
    위젯에서 ItemList의 메서드를 사용할 수 있게 리플리케이션함.

--- HorrorCore/Source/HorrorSystem/Private/Widget_ItemList.cpp
+++ HorrorCore/Source/HorrorSystem/Private/Widget_ItemList.cpp
@@ -9,3 +9,26 @@
 #include "Blueprint/WidgetTree.h"
 #include "Engine/TextureRenderTarget2D.h"
 #include "Widget_ItemSlot.h"
+
+int32 UWidget_ItemList::GetSlotOffset(const int32 SlotIndex) const
+{
+	return SlotIndex * ItemSlotForm.SquareSize + (SlotIndex + 1) * ItemSlotForm.Padding;
+}
+
+FVector2D UWidget_ItemList::GetSlotOffset2D(const int32 X, const int32 Y) const
+{
+	return FVector2D(GetSlotOffset(X), GetSlotOffset(Y));
+}
+
+FVector2D UWidget_ItemList::GetCanvasSize(const int32 X, const int32 Y) const
+{
+	return FVector2D(
+		X * ItemSlotForm.SquareSize + (X + 1) * ItemSlotForm.Padding,
+		Y * ItemSlotForm.SquareSize + (Y + 1) * ItemSlotForm.Padding
+	);
+}
+
+FVector2D UWidget_ItemList::GetCanvasSize() const
+{
+	return GetCanvasSize(ItemListSize.X, ItemListSize.Y);
+}
