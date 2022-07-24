4bf452b1e70ec89c11d7b4711d221ae96ace6db0
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-07-24 19:19:32 +0900

    HorrorCoreEditor에 스트럭트를 생성하고 TestDataTable를 생성함

---
+++ HorrorCoreEditor/Source/HorrorCoreEditor/Public/Horror2DInventoryStructTest.h
@@ -0,0 +1,50 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "HorrorItem.h"
+#include "Horror2DInventoryStruct.h"
+#include "Horror2DInventoryStructTest.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EACTION : uint8
+{
+	NONE,
+	STORE,
+	TAKE,
+};
+
+USTRUCT(BlueprintType)
+struct FWorkUnit
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UHorrorItem* HorrorItem;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FIntPoint Index;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EACTION Action;
+};
+
+USTRUCT(BlueprintType)
+struct FInventory2DInsertTest : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString TestName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FHorror2DInventoryStruct InventoryInfo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<FWorkUnit> WorkUnit;
+};
