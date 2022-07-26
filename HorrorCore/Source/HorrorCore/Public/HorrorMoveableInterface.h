bb098f45cb7ab77fe61877c1df93ede69fe7b1dd
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-07-26 14:40:25 +0900

    ApplyMoving을 추가함
    Moving은 해당 shape과의 intersection location에 의해 결정되게 함으로써, 플레이어로 부터의 움직임뿐만 아니라, 논플레이어로 부터 발생하는 움직임도 사용할 수 있게 합니다.

--- HorrorCore/Source/HorrorCore/Public/HorrorMoveableInterface.h
+++ HorrorCore/Source/HorrorCore/Public/HorrorMoveableInterface.h
@@ -19,13 +19,11 @@ class HORRORCORE_API IHorrorMoveableInterface
 
 public:
 	virtual void PreMoveable() {}
+
+	virtual FVector GetIntersectionPoint(const FVector& Origin, const FVector& Direct) const = 0;
+	virtual void ApplyMoving(const FVector& IntersectionLocation) = 0;
+
 	virtual void PostMoveable() {}
-
-	//virtual void ApplyAxisMovealbe(USceneComponent* Target, const FVector2D& MouseInput) = 0;
-	//
-	//virtual void ApplyAxisMoveable(const FVector& Origin, const FVector& Direction) {}
-
-	//virtual FVector GetIntersectionPoint(const FVector& Origin, const FVector& Direction) const { return FVector(); }
 
 	//virtual FTransform GetNewReleativeTransform(const FVector& OutClosestPoint) const { return FTransform(); }
 	//virtual FTransform ClampTransform(const FTransform& Transform) const { return FTransform(); }
