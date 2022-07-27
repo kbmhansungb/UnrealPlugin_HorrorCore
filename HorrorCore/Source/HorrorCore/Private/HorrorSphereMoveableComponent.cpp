b84c606fab9a131a694d36d03fc215090aeff70d
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-07-27 17:06:33 +0900

    HorrorSphereMoveableComponent의 불필요한 코드를 제거함
    HorrorMovingComponent에 의해 움직임이 관리되므로, 더 Mouse이벤트를 직접 입력하는 방법은 더 이상 필요하지 않음.

--- HorrorCore/Source/HorrorCore/Private/HorrorSphereMoveableComponent.cpp
+++ HorrorCore/Source/HorrorCore/Private/HorrorSphereMoveableComponent.cpp
@@ -10,45 +10,6 @@
 
 #include "DrawDebugHelpers.h"
 
-UHorrorAxisRotationComponent::UHorrorAxisRotationComponent()
-{
-	PrimaryComponentTick.bCanEverTick = true;
-}
-
-void UHorrorAxisRotationComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	TArray<USceneComponent*> Children;
-	GetChildrenComponents(true, Children);
-
-	for (USceneComponent* Child : Children)
-	{
-		if (UPrimitiveComponent* Primitive = Cast<UPrimitiveComponent>(Child))
-		{
-			FScriptDelegate ClickedMouse;
-			ClickedMouse.BindUFunction(this, FName("ClickedMouse"));
-			Primitive->OnClicked.Add(ClickedMouse);
-
-			FScriptDelegate ReleaseMouse;
-			ReleaseMouse.BindUFunction(this, FName("ReleasedMouse"));
-			Primitive->OnReleased.Add(ReleaseMouse);
-		}
-	}
-
-	SetComponentTickEnabled(false);
-}
-
-void UHorrorAxisRotationComponent::SetComponentTickEnabled(bool bEnabled)
-{
-	Super::SetComponentTickEnabled(bEnabled);
-
-	if (!bEnabled)
-	{
-		Key = FKey();
-	}
-}
-
 void UHorrorAxisRotationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
@@ -60,38 +21,6 @@ void UHorrorAxisRotationComponent::TickComponent(float DeltaTime, ELevelTick Tic
 	{
 		SetComponentTickEnabled(false);
 	}
-}
-
-void UHorrorAxisRotationComponent::ClickedMouse(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed)
-{
-	SetComponentTickEnabled(true);
-	Key = ButtonPressed;
-
-	APlayerController* PC = UGameplayStatics::GetPlayerController(this, 0);
-
-	FRay Ray = GetNowMouseRay(PC);
-	FHitResult Hit;
-	FCollisionQueryParams Params;
-	Params.AddIgnoredActor(PC->GetPawn());
-	GetWorld()->LineTraceSingleByChannel(Hit, Ray.Origin, Ray.Origin + Ray.Direction * 1000.f, ECollisionChannel::ECC_Camera, Params);
-
-	//PreAxisMoveable(Hit);
-}
-
-void UHorrorAxisRotationComponent::ReleasedMouse(UPrimitiveComponent* TouchedComponent, FKey ButtonPressed)
-{
-	SetComponentTickEnabled(false);
-}
-
-FRay UHorrorAxisRotationComponent::GetNowMouseRay(APlayerController* PC)
-{
-	FRay Ray;
-	FVector2D MousePosition;
-
-	PC->GetMousePosition(MousePosition.X, MousePosition.Y);
-	UGameplayStatics::DeprojectScreenToWorld(PC, MousePosition, Ray.Origin, Ray.Direction);
-
-	return Ray;
 }
 
 void UHorrorAxisRotationComponent::PreMoveable() 
