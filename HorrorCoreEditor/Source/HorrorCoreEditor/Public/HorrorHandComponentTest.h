0fe9118d6f742ba60b3b5dc807cafb0bf11d764e
kbmhansungb <56149613+kbmhansungb@users.noreply.github.com>
2022-08-05 14:41:34 +0900

    테스트를 위한 UTestHorrorHoldableObject를 구현

--- HorrorCoreEditor/Source/HorrorCoreEditor/Public/HorrorHandComponentTest.h
+++ HorrorCoreEditor/Source/HorrorCoreEditor/Public/HorrorHandComponentTest.h
@@ -3,4 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "HorrorHoldableInterface.h"
 #include "HorrorHandComponent.h"
+#include "HorrorHandComponentTest.generated.h"
+
+UCLASS()
+class HORRORCOREEDITOR_API UTestHorrorHoldableObject : public UObject,
+	public IHorrorHoldableInterface
+{
+	GENERATED_BODY()
+
+public:
+};
